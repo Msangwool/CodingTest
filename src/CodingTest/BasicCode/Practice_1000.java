dd7fb0833c0d37c7fc5137b0a74d8abe7770939e
Erick <97933061+Msangwool@users.noreply.github.com>
2022-08-13 20:51:15 +0900

    Initial commit

---
+++ src/CodingTest/BasicCode/Practice_1000.java
@@ -0,0 +1,16 @@
+package CodingTest.BasicCode;
+
+import java.util.Scanner;
+
+public class Practice_1000 {
+    public static void main(String[] args){
+        int a,b;
+
+        Scanner sc = new Scanner(System.in);
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.print(a+b);
+    }
+}
