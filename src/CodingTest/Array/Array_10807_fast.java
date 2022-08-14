bac9af2cd46a23013f6da3a28038e1e5c19f1d11
Erick <97933061+Msangwool@users.noreply.github.com>
2022-08-14 16:41:39 +0900

    add file

---
+++ src/CodingTest/Array/Array_10807_fast.java
@@ -0,0 +1,31 @@
+package CodingTest.Array;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Array_10807_fast {
+    public static void main(String[] args) throws IOException{
+        int test;
+        int count = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        test = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            if(N>100 | N<0){
+                break;
+            }
+            int a = Integer.parseInt(st.nextToken());
+            if(a<-100 | a>100 | test>100 | test<-100){
+                break;
+            }
+            else if(a == test){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
