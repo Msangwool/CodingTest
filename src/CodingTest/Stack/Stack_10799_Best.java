4edf707db7c1bbe819df38ea90b8a437eadc08e2
Erick <97933061+Msangwool@users.noreply.github.com>
2022-09-01 00:32:52 +0900

    add file

---
+++ src/CodingTest/Stack/Stack_10799_Best.java
@@ -0,0 +1,27 @@
+package CodingTest.Stack;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Stack_10799_Best {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
+        String str = br.readLine();
+        int sum=0;
+        for(int i=0; i<str.length(); i++){
+            if (str.charAt(i) == '(') {
+                stack.push('(');
+            }
+            else if (str.charAt(i) == ')') {
+                stack.pop();
+                if (str.charAt(i-1) == '(') {
+                    sum += stack.size();
+                } else {
+                    sum++;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
