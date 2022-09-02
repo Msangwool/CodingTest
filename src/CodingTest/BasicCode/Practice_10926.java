7f409af9317e7199a3bd2973b0ecfcdc40a8a0d7
Erick <97933061+Msangwool@users.noreply.github.com>
2022-09-02 20:26:54 +0900

    add file

---
+++ src/CodingTest/BasicCode/Practice_10926.java
@@ -0,0 +1,13 @@
+package CodingTest.BasicCode;
+
+import java.io.*;
+
+public class Practice_10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        bw.write(str + "??!");
+        bw.close();
+    }
+}
