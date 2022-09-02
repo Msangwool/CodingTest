e618fee63202fbe4d8a87f776b4b0f91a0fc59ee
Erick <97933061+Msangwool@users.noreply.github.com>
2022-09-02 20:26:08 +0900

    add file

---
+++ src/CodingTest/BasicCode/Practice_3003.java
@@ -0,0 +1,22 @@
+package CodingTest.BasicCode;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Practice_3003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = {1, 1, 2, 2, 2, 8};
+        int number;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb  = new StringBuilder();
+        for(int i=0; i<arr.length; i++){
+            number = Integer.parseInt(st.nextToken());
+            if(number>10 | number<0){
+                System.exit(0);
+            }
+            sb.append(arr[i]-number).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
