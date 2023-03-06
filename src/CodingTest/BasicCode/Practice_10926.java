5be0199e4d21496a42ed681cdda5911cea70929d
OpenTheDoor <tkddnwkdb@gmail.com>
2023-03-07 00:31:15 +0900

    feat: Add Practice_10926

--- src/CodingTest/BasicCode/Practice_10926.java
+++ src/CodingTest/BasicCode/Practice_10926.java
@@ -3,10 +3,14 @@ package CodingTest.BasicCode;
 import java.io.*;
 
 public class Practice_10926 {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         String str = br.readLine();
+
         bw.write(str + "??!");
         bw.close();
     }
