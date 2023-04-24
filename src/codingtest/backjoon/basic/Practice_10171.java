27f1f51719ac487b28764769e3aa7a1ffcdf7642
OpenTheDoor <tkddnwkdb@gmail.com>
2023-04-25 03:37:53 +0900

    refactor: Practice_10171 Improve Performance

--- src/codingtest/backjoon/basic/Practice_10171.java
+++ src/codingtest/backjoon/basic/Practice_10171.java
@@ -2,12 +2,12 @@ package codingtest.backjoon.basic;
 
 public class Practice_10171 {
     public static void main(String[] args){
-        System.out.print("\\    /\\");
-        System.out.println();
-        System.out.print(" )  ( \')");
-        System.out.println();
-        System.out.print("(  /  )");
-        System.out.println();
-        System.out.print(" \\(__)|");
+
+        String sb = "\\    /\\" + "\n" +
+                " )  ( \')" + "\n" +
+                "(  /  )" + "\n" +
+                " \\(__)|" + "\n";
+
+        System.out.println(sb);
     }
 }
