b79f11da148329022df5dc2a040315d4acd63512
OpenTheDoor <tkddnwkdb@gmail.com>
2023-05-04 00:52:44 +0900

    refactor: Practice_2587 Improve Performance

--- src/codingtest/backjoon/basic/Practice_2587.java
+++ src/codingtest/backjoon/basic/Practice_2587.java
@@ -1,28 +1,33 @@
 package codingtest.backjoon.basic;
 
+import java.io.*;
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class Practice_2587 {
-    public static void main(String[] args){
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         int[] arr = new int[5];
         int sum=0;
         int mid;
 
-        Scanner sc = new Scanner(System.in);
-
         for(int i=0; i<arr.length; i++){
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         for(int i=0; i<arr.length; i++){
             sum = sum + arr[i];
         }
 
-        System.out.println(sum/5);
+        sb.append(sum / 5).append("\n");
 
         Arrays.sort(arr);
 
-        System.out.println(arr[2]);
+        sb.append(arr[2]);
+
+        System.out.println(sb);
     }
 }
