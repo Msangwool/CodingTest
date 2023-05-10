a24a1375cc3e429c8ef476a90f9895d85480fa79
OpenTheDoor <tkddnwkdb@gmail.com>
2023-05-10 19:01:17 +0900

    refactor: Practice_3052 Improve Performance

--- src/codingtest/backjoon/basic/Practice_3052.java
+++ src/codingtest/backjoon/basic/Practice_3052.java
@@ -1,24 +1,29 @@
 package codingtest.backjoon.basic;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Practice_3052 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
 
         int[] arr = new int[10];
         int c, j=0, count=1;
 
         for(int i =0; i<10; i++){
-            c = sc.nextInt();
+            c = Integer.parseInt(br.readLine());
 
             if(c>1000 | c<0){
                 System.exit(0);
             }
             arr[i] = (c%42);
         }
+
         Arrays.sort(arr);
+
         c = arr[j];
         j++;
         while(j<10){
