3169a5a895aa2b2a25194b1e191323f412d07ce7
OpenTheDoor <tkddnwkdb@gmail.com>
2023-05-11 22:14:54 +0900

    refactor: Array_10808 Improve Performance

--- src/codingtest/backjoon/array/Array_10808.java
+++ src/codingtest/backjoon/array/Array_10808.java
@@ -1,16 +1,19 @@
 package codingtest.backjoon.array;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Array_10808 {
-    public static void main(String[] args){
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         int ascii = 97;
         int[] alpabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         int[] alpabetPrint = new int[26];
 
-        Scanner sc = new Scanner(System.in);
-
-        String str = sc.nextLine();
+        String str = br.readLine();
 
         if(str.length() > 100){
             System.exit(0);
@@ -22,20 +25,9 @@ public class Array_10808 {
                     alpabetPrint[i]++;
                 }
             }
-            System.out.print(alpabetPrint[i] + " ");
+            sb.append(alpabetPrint[i]).append(" ");
         }
-//        Scanner sc = new Scanner(System.in);
-//
-//        String str = sc.nextLine();
-//        String[] strArr = str.split("");
-//
-//        for(int i=0; i<alpabet.length; i++){
-//            for(int j = 0; j<strArr.length; j++){
-//                if(Integer.parseInt(strArr[j]) == ascii){
-//                    alpabet[i]++;
-//                }
-//            }
-//            ascii++;
-//        }
+
+        System.out.println(sb);
     }
 }
