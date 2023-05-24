3a0b410f3ed5a78c5bcdb753530514d23db4f236
OpenTheDoor <tkddnwkdb@gmail.com>
2023-05-24 19:46:30 +0900

    feat: Add CountingSort1

---
+++ src/codingtest/hackerrank/InterviewPreparationKits1/CountingSort1.java
@@ -0,0 +1,55 @@
+package codingtest.hackerrank.InterviewPreparationKits1;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class CountingSort1 {
+
+    static class Result {
+
+        /*
+         * Complete the 'countingSort' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static List<Integer> countingSort(List<Integer> arr) {
+            // Write your code here
+
+            int[] range = new int[100];
+
+            arr.forEach(num -> range[num]++);
+
+            return Arrays.stream(range).boxed().collect(Collectors.toList());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.countingSort(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
