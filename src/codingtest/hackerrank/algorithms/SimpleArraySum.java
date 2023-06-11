10c03db16fe4a2b39b7d96c82461ce153f7f8d96
OpenTheDoor <tkddnwkdb@gmail.com>
2023-06-11 14:55:24 +0900

    feat: Add SimpleArraySum

---
+++ src/codingtest/hackerrank/algorithms/SimpleArraySum.java
@@ -0,0 +1,48 @@
+package codingtest.hackerrank.algorithms;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class SimpleArraySum {
+
+    static class Result {
+
+        /*
+         * Complete the 'simpleArraySum' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY ar as parameter.
+         */
+
+        public static int simpleArraySum(List<Integer> ar) {
+            // Write your code here
+            Integer sum = 0;
+
+            for (Integer integer : ar) {
+                sum += integer;
+            }
+
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .toList();
+
+        int result = Result.simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
