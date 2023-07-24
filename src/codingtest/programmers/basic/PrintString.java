9e1cf8d33dfe858fc181c6fcb1655041aeb7b7f5
OpenTheDoor <tkddnwkdb@gmail.com>
2023-07-25 00:08:21 +0900

    feat: Add PrintString

---
+++ src/codingtest/programmers/basic/PrintString.java
@@ -0,0 +1,12 @@
+package codingtest.programmers.basic;
+
+import java.util.Scanner;
+
+public class PrintString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+}
