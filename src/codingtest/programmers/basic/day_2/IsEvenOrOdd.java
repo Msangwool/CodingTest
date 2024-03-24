6c4b21028c5131f8d5d36d92195b512a1902ba91
OpenTheDoor <tkddnwkdb@gmail.com>
2024-03-24 22:53:52 +0900

    refactor: Modify IdEvenOrOdd

--- src/codingtest/programmers/basic/day_2/IsEvenOrOdd.java
+++ src/codingtest/programmers/basic/day_2/IsEvenOrOdd.java
@@ -1,12 +1,16 @@
-package codingtest.programmers.basic;
+package codingtest.programmers.basic.day_2;
 
 import java.util.Scanner;
 
 public class IsEvenOrOdd {
     public static void main(String[] args) {
+        // input
+        // 2
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         System.out.println(n%2 == 0 ? n + " is even" : " is odd");
+        // output
+        // 2 is even
     }
 }
