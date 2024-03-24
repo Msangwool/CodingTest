4fbc65920de9fa31409f14033869794696a485f2
OpenTheDoor <tkddnwkdb@gmail.com>
2024-03-24 22:44:30 +0900

    feat: Add IdEvenOrOdd

---
+++ src/codingtest/programmers/basic/IsEvenOrOdd.java
@@ -0,0 +1,12 @@
+package codingtest.programmers.basic;
+
+import java.util.Scanner;
+
+public class IsEvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n%2 == 0 ? n + " is even" : " is odd");
+    }
+}
