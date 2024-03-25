2b190b7b206a4a3b979d9d8a7e881a2d81e29d53
OpenTheDoor <tkddnwkdb@gmail.com>
2024-03-25 21:57:51 +0900

    feat: Add TransformString

---
+++ src/codingtest/programmers/basic/TransformString.java
@@ -0,0 +1,17 @@
+package codingtest.programmers.basic;
+
+import java.util.Scanner;
+
+public class TransformString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        String answer = String.valueOf(n);
+        System.out.println(answer);
+    }
+
+}
