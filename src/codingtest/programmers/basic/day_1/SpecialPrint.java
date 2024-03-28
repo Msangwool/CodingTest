a807154ab77755c42bb2cd5d99dec2a76e59f236
OpenTheDoor <tkddnwkdb@gmail.com>
2024-03-29 00:43:51 +0900

    feat: Add SpecialPrint

---
+++ src/codingtest/programmers/basic/day_1/SpecialPrint.java
@@ -0,0 +1,8 @@
+package codingtest.programmers.basic.day_1;
+
+public class SpecialPrint {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+        // output -> !@#$%^&*(\'"<>?:;
+    }
+}
