ad0cae88027f4814622594b8be15b501cb261ff6
OpenTheDoor <tkddnwkdb@gmail.com>
2024-04-09 01:02:37 +0900

    feat: Add SequenceAndSectionSolving4

---
+++ src/codingtest/programmers/basic/day_7/SequenceAndSectionSolving4.java
@@ -0,0 +1,21 @@
+package codingtest.programmers.basic.day_7;
+
+public class SequenceAndSectionSolving4 {
+
+
+    public int[] solution(int[] arr, int[][] queries) {
+
+        for (int[] query : queries) {
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+
+            for (int i = s; i <= e; i++) {
+                if (i % k == 0) {
+                    arr[i]++;
+                }
+            }
+        }
+        return arr;
+    }
+}
